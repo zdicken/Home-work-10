58e6605936bf236e96897d07dad57159a7840cca
zdicken <78879218+zdicken@users.noreply.github.com>
2021-04-18 22:48:26 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var clicked = false;
+
+$(".dropDownMenu").click(function() {
+   $(".navWrapper").toggleClass("toggle");
+   clicked=!clicked;
+});
+
+$(window).resize(function(){
+   if(window.innerWidth>=600&&$(".navWrapper").hasClass("toggle")) {
+       $(".navWrapper").removeClass("toggle");
+   } else if(window.innerWidth<600&&clicked) {
+       $(".navWrapper").addClass("toggle");
+   }
+});
